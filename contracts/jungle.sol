72dd73b8bacb382f8f5bc24256d93b1eb133f2ca
matellordson <snr.mn.lordson@gmail.com>
2025-06-14 07:43:03 +0100

    updated procuct contract with simple functions

--- contracts/jungle.sol
+++ contracts/jungle.sol
@@ -4,6 +4,7 @@ pragma solidity <0.9.0;
 contract Products {
 
     struct Product{
+        address publisher;
         string media;
         string title;
         string description;
@@ -12,7 +13,17 @@ contract Products {
 
     Product[] public product;
 
-    function addProduct() public {
+    function addProduct(address _publisher ,string memory _media, string memory _title, string memory _description, string memory _price) public {
+        product.push(Product({ 
+            publisher: _publisher,
+            media: _media,
+            title: _title,
+            description: _description,
+            price: _price
+        }));
+    }
 
+    function getProduct() public view returns ( Product[] memory) {
+        return product;
     }
 }
